686dbbf011d4d76a09ac7b3f856af6adbfcf1e2c
HLNoll <hyuryluis@gmail.com>
2019-06-22 11:36:33 -0300

    Novas features para os usuarios

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -2,16 +2,19 @@ const Usuario = require('../models/usuario');
 const Imovel = require('../models/imovel');
 
 module.exports = {
+
+    //metodo que busca todos os usuarios do banco de dados
     async getAll(req, res){
         const usuario = await Usuario.find();
         return res.json(usuario)
     },
 
+    
+
+    //metodo que faz o registro de um usuario no banco de dados
     async post(req, res){
         const {nome, sobrenome, email, senha, endereco, cpf, telefone} = req.body;
-
-
-       const usuario = await Usuario.create({
+        const usuario = await Usuario.create({
             nome,
             sobrenome,
             email,
@@ -24,16 +27,23 @@ module.exports = {
         return res.json(usuario)
     },
 
+
+
+    //busca um usuario utilzando o id como parametro de busca
     async getOne(req, res){
         const usuario = await Usuario.findById(req.params.id)
         return res.json(usuario);
     },
 
+
+    //busca um usuario utilizando o cpf como parametro
     async getOneByCpf(req, res){
         const usuario = await Usuario.findOne({cpf : `${req.params.cpf}`})
         res.json(usuario);
     },
 
+
+    //registra no banco o id do imovel que o usuario tem como favorito
     async favoritarImovel(req, res){
         const usuario = await Usuario.findById(req.params.id);
         usuario.imoveisFavoritos.push(req.params.idImovel);
@@ -45,6 +55,8 @@ module.exports = {
         res.json(usuario);
     },
 
+
+    //remove um imovel dos favoritos
     async removerImovelFavorito(req, res){
         const usuario = await Usuario.findById(req.params.id);
         usuario.imoveisFavoritos.remove(req.params.idImovel);
@@ -56,20 +68,57 @@ module.exports = {
         res.json(usuario);
     },
 
+   
+    //deleta um usuario pelo id
     async delete(req, res){
         let usuario = await Usuario.findByIdAndDelete(req.params.id);
         res.json(usuario);
     },
 
-    async update(req, res){
 
+
+    //atualiza as informações do usuario
+    async update(req, res){
         let usuario = await Usuario.findOneAndUpdate(req.params.id,req.body,{new : true});
 
         res.json(usuario);
-    }
+    },
     
 
+    //este metodo faz a validação de email e senha no banco de dados e retorna um json de usuario
+    async checkPassword(req, res){
+        let retornoJson = {
+            status: Boolean,
+            usuario: Object
+        }
+        await Usuario.findOne({email : req.body.email, senha : req.body.senha, cpf: req.body.cpf}).then(
+            usuarioReq => {
+                if(usuarioReq == null){
+                    retornoJson.status = false
+                    return res.json(retornoJson);
+                }
 
+                else{
+                    retornoJson.status = true;
+                    retornoJson.usuario = usuarioReq;
+                    return res.json(retornoJson);
+                }
+            }
+        )
+    },
     
+
+    //verifica se o imovel é um favorito
+    async verificarImovelFavorito(req, res){ 
+        await Usuario.findById(req.params.id).then(
+            usuario =>{
+                usuario.imoveisFavoritos.filter( obj =>{
+                    return res.send(obj._id == req.params.idImovel);
+                });
+            }
+        )
+    },
+
+
 
 }
--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -7,13 +7,15 @@ const routes = new express.Router();
 
 //ROUTES
 routes.post('/usuario',usuarioController.post);
-routes.get('/usuario',usuarioController.getAll)
+routes.get('/usuario',usuarioController.getAll);
 routes.get('/usuario/cpf/:cpf', usuarioController.getOneByCpf);
 routes.get('/usuario/byId/:id',usuarioController.getOne);
-routes.post('/usuario/:id/:idImovel', usuarioController.favoritarImovel)
-routes.delete('/usuario/:id/:idImovel', usuarioController.removerImovelFavorito)
-routes.delete('/usuario/:id',usuarioController.delete)
-routes.put('/usuario/:id', usuarioController.update)
+routes.post('/usuario/:id/:idImovel', usuarioController.favoritarImovel);
+routes.delete('/usuario/:id/:idImovel', usuarioController.removerImovelFavorito);
+routes.delete('/usuario/:id',usuarioController.delete);
+routes.put('/usuario/:id', usuarioController.update);
+routes.get('/usuario/pass', usuarioController.checkPassword);
+routes.get('/usuario/imoveis/:id/:idImovel', usuarioController.verificarImovelFavorito);
 
 module.exports = routes;
 
